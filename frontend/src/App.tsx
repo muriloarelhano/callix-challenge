e9c0c9108e086e55b62d173e681c3a7fbe0df62c
Murilo Esteca Arelhano <51941771+muriloarelhano@users.noreply.github.com>
2022-10-13 14:55:57 -0400

    feat: create basic react project with npx create react app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div>
+      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+    </div>
+  );
+}
+
+export default App;
